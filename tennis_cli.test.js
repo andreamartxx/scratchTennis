3c09270690b1fa328db67ee347c2ced1a444107a
Andrea Martin <amartin.onda@gmail.com>
2022-04-11 10:23:27 +0200

    Pass the test for the cli tennis

---
+++ tennis_cli.test.js
@@ -0,0 +1,14 @@
+const printFunction = require('./tennis_cli');
+
+describe('cli tennis score kata', ()=>{
+
+    test('player test score', ()=>{
+        const prompt = jest.fn();
+        const log = jest.fn();
+        const console = {log: log};
+        prompt.mockReturnValueOnce(1);
+        printFunction(prompt, console);
+        expect(prompt).toHaveBeenCalledWith('Which player scores? ');
+        expect(log).toHaveBeenCalledWith('15 0');
+    });
+});
