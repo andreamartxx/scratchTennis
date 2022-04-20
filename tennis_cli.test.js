0bf24e582c82ff87e78a4ea1d35d3d5dc7468478
Andrea Martin <amartin.onda@gmail.com>
2022-04-20 08:04:27 +0200

    testing loop

--- tennis_cli.test.js
+++ tennis_cli.test.js
@@ -6,9 +6,23 @@ describe('cli tennis score kata', ()=>{
         const prompt = jest.fn();
         const log = jest.fn();
         const console = {log: log};
+        const loopMock = jest.fn();
+        loopMock
+            .mockReturnValueOnce('15 0')
+            .mockReturnValueOnce('30 0')
+            .mockReturnValueOnce('40 0')
+            .mockReturnValueOnce('win 0')
+            .mockReturnValue('game over');
+        
         prompt.mockReturnValueOnce(1);
         printFunction(prompt, console);
         expect(prompt).toHaveBeenCalledWith('Which player scores? ');
         expect(log).toHaveBeenCalledWith('15 0');
+
+        expect(loopMock).toHaveBeenCalledWith(1, '15 0');
+        expect(loopMock).toHaveBeenCalledWith(2, '30 0');
+        expect(loopMock).toHaveBeenCalledWith(3, '40 0');
+        expect(loopMock).toHaveBeenCalledWith(4, 'win 0');
+        expect(loopMock).toHaveBeenCalledWith(5, 'game over');
     });
 });
